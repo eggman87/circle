552c1c574dfe30a3154f70bb863dab840f836bcf
a765904 <mharris@itriagehealth.com>
2015-06-24 21:27:41 -0600

    adding circle methods.

--- lib/circle.rb
+++ lib/circle.rb
@@ -6,22 +6,21 @@
 class Circle
 
 	def initialize(rad)
-		# add code here
+		@radius = rad
 	end
 
-	# your code here
+	def radius()
+		#not using return as it seems that is not desirable in rubyworld.
+		@radius
+	end
 
-	# def draw
-	# 	img = Image.new(radius*2+100,radius*2+100) { self.background_color = "white" }
-	# 	d = Magick::Draw.new
-	# 	d.stroke = "red"
-	# 	d.fill_opacity(0)
-	# 	d.stroke_width(6)
-	# 	d.ellipse(img.rows/2,img.columns/2, radius, radius, 0, 360)
-	# 	d.draw(img)
-	# 	img.display
-	# end
+	def area()
+		Math::PI * (@radius ^ 2)
+	end
 
+	def circumference()
+		(2 * Math::PI * @radius)
+	end
 end
 
 if __FILE__ == $0
