e8a6214b4ce3bfe8c5609ff022ddb0283710c799
a765904 <mharris@itriagehealth.com>
2015-06-25 14:54:07 -0600

    - changes after class #2.

--- lib/circle.rb
+++ lib/circle.rb
@@ -2,24 +2,33 @@
 
 # require 'RMagick'
 # include Magick
-
+# class naming is CamelCase, methods and properties are snake_case
 class Circle
 
+	#shortcut for auto gen getter/setter...attr_accessor
+	attr_accessor :radius
+
 	def initialize(rad)
-		@radius = rad
+		self.radius = rad
 	end
 
-	def radius()
-		#not using return as it seems that is not desirable in rubyworld.
-		@radius
+	#getter
+	# def radius
+	# 	#not using return as it seems that is not desirable in rubyworld.
+	# 	@radius
+	# end
+  #
+	# #setter, using equals sign.
+	# def radius=(rad)
+	# 	@radius = rad
+	# end
+
+	def area
+		Math::PI * (self.radius ^ 2)
 	end
 
-	def area()
-		Math::PI * (@radius ^ 2)
-	end
-
-	def circumference()
-		(2 * Math::PI * @radius)
+	def circumference
+		2 * Math::PI * self.radius
 	end
 end
 
